c7a9aa3d20850bbcc7dbfd94c8046b9461da0b30
Ryu Jun <ironbike@live.co.kr>
2021-12-14 21:48:31 +0900

    초기 커밋.

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int a = 0;
+	printf("Hello World!!\n");
+	
+	scanf("%d", &a);
+	printf("%d", a);
+
+	return 0;
+}
+
+// 이제 나이들어서 10 포인트 글꼴은 불편하다구!
